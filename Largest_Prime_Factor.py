06bf389960cc23cd009ed258814a4a1bc9e24cf8
Jacob Budzis <jrbudzis@alumni.ubc.ca>
2016-01-27 20:32:38 -0700

    Solved

--- Largest_Prime_Factor.py
+++ Largest_Prime_Factor.py
@@ -1,27 +1,23 @@
 from time import clock
 
 start = clock()
-number = 13195 #600851475143
-primes = set(range(2,number+1))   #This is killing my computer -> Makes an exclusion list instead
+number = 600851475143
 primeFactors = []
 i = 1
 print(number)
 
-# Generate all primes - Sieve of Erathosthenes
+# We factor our number from lower values -> higher values
+# This means that each lowest prime will eliminate higher composites 
+# --> A 2 factored out will simplify composites ( 6 = 2*3 -> 3)
+# We will loop this structure until we finish
+# This method relates to the Sieve of Eratosthenes
+
 while (i < number):
     i += 1
-    if i in primes:  # Determines if this value is prime (This algorithm will remove non-primes as it proceeds)
-        if (number % i == 0):  # Prime factor of our number
-            while (number % i == 0):
-                primeFactors.append(i)
-                number = number/i
-                print(number)
-            newSet = set(range(2,int(number+1)))            
-            primes = primes & newSet # Uses the new number to vastly shorten the primes we must look through.                        
-            j = 2
-            while (i*j<=number):  # Now we remove multiples of our primes. The lowest non-removed value becomes a prime automatically (no factors underneath it)
-                primes.discard(i*j)
-                j += 1        
+    if (number % i == 0):  # Factor of our number 
+        while (number % i == 0): #Multiple factors
+            primeFactors.append(i)
+            number = number/i
 
 print(primeFactors)
 print('Max:',max(primeFactors))
